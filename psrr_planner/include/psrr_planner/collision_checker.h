21de442163a308e591e01fbc7c2e5d46115aa82b
mlsdpk <mlsdphonethk@gmail.com>
2021-08-04 03:29:22 +0630

    Create grid collision detector

--- psrr_planner/include/psrr_planner/collision_checker.h
+++ psrr_planner/include/psrr_planner/collision_checker.h
@@ -1,19 +1,50 @@
+/******************************************************************************
+Copyright (c) 2021, Phone Thiha Kyaw. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
 #pragma once
 
 #include <costmap_2d/costmap_2d.h>
 #include <costmap_2d/costmap_2d_ros.h>
+#include <costmap_2d/footprint.h>
 #include <geometry_msgs/Polygon.h>
+#include <psrr_planner/line_iterator.h>
 
 namespace psrr_planner {
+
+static const unsigned char NO_INFORMATION = 255;
+static const unsigned char LETHAL_OBSTACLE = 254;
+static const unsigned char INSCRIBED_INFLATED_OBSTACLE = 253;
+static const unsigned char FREE_SPACE = 0;
+
 class GridCollisionChecker {
  public:
   /**
    * @brief A constructor for psrr_planner::GridCollisionChecker
    * @param costmap The costmap to collision check against
    */
-  GridCollisionChecker(costmap_2d::Costmap2D* costmap) {
-    //
-  }
+  GridCollisionChecker(costmap_2d::Costmap2D* costmap) : costmap_{costmap} {}
 
   /**
    * @brief Check if the footprint is in collision with the current shared
@@ -25,10 +56,91 @@ class GridCollisionChecker {
    * @return boolean if in collision or not.
    */
   bool isCollision(const double x, const double y, const double theta,
-                   const geometry_msgs::Polygon& footprint) {
-    //
+                   const std::vector<geometry_msgs::Point>& footprint) {
+    // always check the cell corrdinate of the center of the robot
+    unsigned int cell_x, cell_y;
+    if (!costmap_->worldToMap(x, y, cell_x, cell_y)) return true;
+
+    unsigned char cost = costmap_->getCost(cell_x, cell_y);
+    if (cost == NO_INFORMATION || cost == LETHAL_OBSTACLE ||
+        cost == INSCRIBED_INFLATED_OBSTACLE)
+      return true;
+
+    // now we need to check the full polygon footprint of the robot
+
+    // create a new footprint by transforming the current one into desired pose
+    std::vector<geometry_msgs::Point> transformed_footprint;
+    costmap_2d::transformFootprint(x, y, theta, footprint,
+                                   transformed_footprint);
+
+    // now use this transformed footprint to check collision in the costmap
+    unsigned int x0, x1, y0, y1;
+    double footprint_cost = 0.0;
+
+    // rasterize each line in the footprint
+    for (unsigned int i = 0; i < transformed_footprint.size() - 1; ++i) {
+      // get the cell coord of the first point
+      if (!costmap_->worldToMap(transformed_footprint[i].x,
+                                transformed_footprint[i].y, x0, y0))
+        return true;
+
+      // get the cell coord of the second point
+      if (!costmap_->worldToMap(transformed_footprint[i + 1].x,
+                                transformed_footprint[i + 1].y, x1, y1))
+        return true;
+
+      footprint_cost = std::max(lineCost(x0, x1, y0, y1), footprint_cost);
+
+      // if in collision, no need to continue
+      if (footprint_cost == static_cast<double>(NO_INFORMATION) ||
+          footprint_cost == static_cast<double>(LETHAL_OBSTACLE) ||
+          footprint_cost == static_cast<double>(INSCRIBED_INFLATED_OBSTACLE)) {
+        return true;
+      }
+    }
+
+    // connect the first point in the footprint to the last point
+    // get the cell coord of the last point
+    if (!costmap_->worldToMap(transformed_footprint.back().x,
+                              transformed_footprint.back().y, x0, y0))
+      return true;
+
+    // get the cell coord of the first point
+    if (!costmap_->worldToMap(transformed_footprint.front().x,
+                              transformed_footprint.front().y, x1, y1))
+      return true;
+
+    footprint_cost = std::max(lineCost(x0, x1, y0, y1), footprint_cost);
+    if (footprint_cost == static_cast<double>(NO_INFORMATION) ||
+        footprint_cost == static_cast<double>(LETHAL_OBSTACLE) ||
+        footprint_cost == static_cast<double>(INSCRIBED_INFLATED_OBSTACLE)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  double lineCost(int x0, int x1, int y0, int y1) const {
+    double line_cost = 0.0;
+    double point_cost = -1.0;
+
+    for (LineIterator line(x0, y0, x1, y1); line.isValid(); line.advance()) {
+      point_cost =
+          pointCost(line.getX(), line.getY());  // Score the current point
+
+      if (point_cost < 0) return point_cost;
+
+      if (line_cost < point_cost) line_cost = point_cost;
+    }
+
+    return line_cost;
+  }
+
+  double pointCost(int x, int y) const {
+    return static_cast<double>(costmap_->getCost(x, y));
   }
 
  private:
+  costmap_2d::Costmap2D* costmap_;
 };
 }  // namespace psrr_planner
--- psrr_planner/src/psrr_planner_nodelet.cpp
+++ psrr_planner/src/psrr_planner_nodelet.cpp
@@ -1,8 +1,45 @@
+/******************************************************************************
+Copyright (c) 2021, Phone Thiha Kyaw. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
+#include <costmap_2d/costmap_2d.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <geometry_msgs/Point.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <nodelet/nodelet.h>
 #include <pluginlib/class_list_macros.h>
 #include <psrr_msgs/Path.h>
+#include <psrr_planner/collision_checker.h>
 #include <ros/ros.h>
+#include <tf2/convert.h>
+#include <tf2/utils.h>
+#include <tf2_ros/transform_listener.h>
+#include <visualization_msgs/MarkerArray.h>
+
+#include <atomic>
+#include <memory>
+#include <mutex>
 
 namespace psrr_planner {
 class PsrrPlannerNodelet : public nodelet::Nodelet {
@@ -42,10 +79,79 @@ class PsrrPlannerNodelet : public nodelet::Nodelet {
     path_pub_timer_ =
         mt_nh_.createWallTimer(ros::WallDuration(path_update_interval),
                                &PsrrPlannerNodelet::pathPublisherTimerCB, this);
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Footprint of self-reconfigurable robots cannot be defined as static like
+    // normal robot. As for now, user needs to provide the dynamically changing
+    // footprint of the robot through ROS services. This planner will request
+    // the footprint of the robot as a service client.
+
+    // footprint_client_ =
+    // mt_nh_.serviceClient<psrr_srvs::FootPrint>("footprint");
+    ////////////////////////////////////////////////////////////////////////////
+
+    // fixed hardcoded footprint for now
+    auto p0 = geometry_msgs::Point();
+    p0.x = 1.025;
+    p0.y = -0.5;
+    auto p1 = geometry_msgs::Point();
+    p1.x = 1.025;
+    p1.y = 0.5;
+    auto p2 = geometry_msgs::Point();
+    p2.x = -1.025;
+    p2.y = 0.5;
+    auto p3 = geometry_msgs::Point();
+    p3.x = -1.025;
+    p3.y = -0.5;
+    footprint_.push_back(p0);
+    footprint_.push_back(p1);
+    footprint_.push_back(p2);
+    footprint_.push_back(p3);
+
+    tf_ = std::make_shared<tf2_ros::Buffer>(ros::Duration(10));
+    tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_);
+
+    planner_costmap_ros_.reset(
+        new costmap_2d::Costmap2DROS("global_costmap", *tf_));
+
+    planner_costmap_ros_->setUnpaddedRobotFootprint(footprint_);
+
+    // create collision checker object
+    // and passes costmap pointer to it
+    collision_checker_.reset(
+        new GridCollisionChecker(planner_costmap_ros_->getCostmap()));
+
+    planner_costmap_ros_->start();
   }
 
  private:
-  void goalCB(const geometry_msgs::PoseStamped& msg) {}
+  void goalCB(const geometry_msgs::PoseStamped& msg) {
+    // get the desired goal pose
+
+    ///////////////////////////////////////
+    // Testing code for collison checking
+    ///////////////////////////////////////
+
+    // we'll test our collision checker based on RVIZ 2D Nav Goal
+
+    std::lock_guard<std::mutex> lock(planner_costmap_mutex_);
+
+    auto init_time = std::chrono::system_clock::now();
+
+    bool is_collision = collision_checker_->isCollision(
+        msg.pose.position.x, msg.pose.position.y,
+        tf2::getYaw(msg.pose.orientation),
+        planner_costmap_ros_->getRobotFootprint());
+
+    auto execution_time = std::chrono::duration_cast<std::chrono::microseconds>(
+                              std::chrono::system_clock::now() - init_time)
+                              .count();
+
+    std::cout << "Collision detection finished in " << execution_time << " us"
+              << std::endl;
+    std::cout << "Collision status: " << is_collision << std::endl;
+    ///////////////////////////////////////
+  }
 
   void plannerTimerCB(const ros::WallTimerEvent& event) {}
 
@@ -64,6 +170,18 @@ class PsrrPlannerNodelet : public nodelet::Nodelet {
 
   ros::WallTimer planner_timer_;
   ros::WallTimer path_pub_timer_;
+
+  ros::ServiceClient footprint_client_;
+
+  std::shared_ptr<tf2_ros::Buffer> tf_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+
+  std::mutex planner_costmap_mutex_;
+  std::unique_ptr<costmap_2d::Costmap2DROS> planner_costmap_ros_;
+
+  std::unique_ptr<GridCollisionChecker> collision_checker_;
+
+  std::vector<geometry_msgs::Point> footprint_;
 };
 }  // namespace psrr_planner
 
